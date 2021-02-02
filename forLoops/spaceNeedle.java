6b9454d1707a499ff4af24852a4b165dd87e44b4
Vinh Mao <vinhxmao87@gmail.com>
2021-02-02 11:17:10 -0800

    spaceNeedle: commented code removed for readability, more comments added

--- forLoops/spaceNeedle.java
+++ forLoops/spaceNeedle.java
@@ -5,7 +5,8 @@ package forLoops;
 public class spaceNeedle {
 
     // Change the number below to change the size of the Space Needle
-    public static final int HEIGHT = 3;
+    // Default HEIGHT is 4
+    public static final int HEIGHT = 4;
     public static void main(String args[]) {
 
         drawPole();
@@ -28,9 +29,7 @@ public class spaceNeedle {
     public static void drawPole() {
         for(int x = 1; x <= HEIGHT; x++) {
             drawObject(3 * HEIGHT, " ");
-            // for(int i = 0; i < 3 * HEIGHT; i++) {
-            //     System.out.print(" ");
-            // }
+
             System.out.print("||");
             System.out.println();
         }
@@ -40,27 +39,19 @@ public class spaceNeedle {
     public static void drawBase() {
         for(int x = 1; x <= HEIGHT; x++) {
             drawObject(3 * HEIGHT - 3 * x, " ");
-            // for(int i = 0; i < 3 * HEIGHT - 3 * x; i++) {
-            //     System.out.print(" ");
-            // }
             System.out.print("__/");
+
             drawObject(-3 + 3 * x, ":");
-            // for(int j = 0; j < -3 + 3 * x; j++) {
-            //     System.out.print(":");
-            // }
             System.out.print("||");
+
             drawObject(-3 + 3 * x, ":");
-            // for(int j = 0; j < -3 + 3 * x; j++) {
-            //     System.out.print(":");
-            // }
             System.out.print("\\__");
+
             System.out.println();
         }
+
         System.out.print("|");
         drawObject(6 * HEIGHT, "\"");
-        // for(int y = 0; y < 6 * HEIGHT; y++) {
-        //     System.out.print('"');
-        // }
         System.out.print("|");
     }
 
@@ -68,16 +59,12 @@ public class spaceNeedle {
     public static void drawBottom() {
         for(int x = 1; x <= HEIGHT; x++) {
             drawObject(-2 + 2 * x, " ");
-            // for(int i = 0; i < -2 + 2 * x; i++) {
-            //     System.out.print(" ");
-            // }
+
             System.out.print("\\_");
 
             // (4, 13) and (5, 16), where (HEIGHT, number of "/\"s to print)
             drawObject(((3 * HEIGHT) + 1) - 2 * x, "/\\");
-            // for(int j = 0; j < ((3 * HEIGHT) + 1) - 2 * x; j++) {
-            //     System.out.print("/\\");
-            // }
+
             System.out.print("_/");
             System.out.println();
         }
@@ -89,9 +76,7 @@ public class spaceNeedle {
 
             // (4, 9) and (5, 12) where (HEIGHT, number of spaces)
             drawObject((3 * HEIGHT) - 3, " ");
-            // for(int i = 0; i < (3 * HEIGHT) - 3; i++) {
-            //     System.out.print(" ");
-            // }
+
             System.out.print("|%%||%%|");
             System.out.println();
         }
