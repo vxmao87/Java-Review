5c408734e1cce247eb717a57c99ce257271abb8e
Vinh Mao <vinhxmao87@gmail.com>
2021-02-02 00:26:08 -0800

    spaceNeedle: created helper method to condense code

--- forLoops/spaceNeedle.java
+++ forLoops/spaceNeedle.java
@@ -1,6 +1,10 @@
 package forLoops;
 
+// This code prints a loose replica of the Space Needle. Size of the figure can
+// be changed by changing the class constant below.
 public class spaceNeedle {
+
+    // Change the number below to change the size of the Space Needle
     public static final int HEIGHT = 7;
     public static void main(String args[]) {
 
@@ -13,39 +17,54 @@ public class spaceNeedle {
         drawBase();
     }
 
+    // Main method for printing characters that make up Space Needle
+    public static void drawObject(int times, String character) {
+        for(int i = 0; i < times; i++) {
+            System.out.print(character);
+        }
+    }
+
+    // Prints the pole of the Space Needle
     public static void drawPole() {
         for(int x = 1; x <= HEIGHT; x++) {
-            for(int i = 0; i < 3 * HEIGHT; i++) {
-                System.out.print(" ");
-            }
+            drawObject(3 * HEIGHT, " ");
+            // for(int i = 0; i < 3 * HEIGHT; i++) {
+            //     System.out.print(" ");
+            // }
             System.out.print("||");
             System.out.println();
         }
     }
 
+    // Prints the base of the Space Needle
     public static void drawBase() {
         for(int x = 1; x <= HEIGHT; x++) {
-            for(int i = 0; i < 3 * HEIGHT - 3 * x; i++) {
-                System.out.print(" ");
-            }
+            drawObject(3 * HEIGHT - 3 * x, " ");
+            // for(int i = 0; i < 3 * HEIGHT - 3 * x; i++) {
+            //     System.out.print(" ");
+            // }
             System.out.print("__/");
-            for(int j = 0; j < -3 + 3 * x; j++) {
-                System.out.print(":");
-            }
+            drawObject(-3 + 3 * x, ":");
+            // for(int j = 0; j < -3 + 3 * x; j++) {
+            //     System.out.print(":");
+            // }
             System.out.print("||");
-            for(int j = 0; j < -3 + 3 * x; j++) {
-                System.out.print(":");
-            }
+            drawObject(-3 + 3 * x, ":");
+            // for(int j = 0; j < -3 + 3 * x; j++) {
+            //     System.out.print(":");
+            // }
             System.out.print("\\__");
             System.out.println();
         }
         System.out.print("|");
-        for(int y = 0; y < 6 * HEIGHT; y++) {
-            System.out.print('"');
-        }
+        drawObject(6 * HEIGHT, "\"");
+        // for(int y = 0; y < 6 * HEIGHT; y++) {
+        //     System.out.print('"');
+        // }
         System.out.print("|");
     }
 
+    // Prints the bottom portion of the head of the Space Needle
     public static void drawBottom() {
         for(int x = 1; x <= HEIGHT; x++) {
             for(int i = 0; i < -2 + 2 * x; i++) {
@@ -62,6 +81,7 @@ public class spaceNeedle {
         }
     }
 
+    // Prints the body of the Space Needle
     public static void drawBody() {
         for(int x = 1; x <= 4 * HEIGHT; x++) {
 
