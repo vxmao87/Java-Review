8cdfb655c56ba1a59ce5e37343b18db5a7fa6c7a
Vinh Mao <vinhxmao87@gmail.com>
2021-02-01 22:44:26 -0800

    slashFigure: method updated, error in output

--- ForLoops.java
+++ ForLoops.java
@@ -5,14 +5,14 @@ public class ForLoops {
 
     public static void slashFigure() {
         for(int x = 0; x < 6; x++) {
-            for(int i = 0; i <= 10; i += 2) {
-                System.out.print("n\\");
-                for(int j = 22; j >= 2; j -=4) {
-                    System.out.print("!");
-                    for(int k = 0; k <= 10; k += 2) {
-                        System.out.print("/");
-                    }
-                }
+            for(int i = -2; i <= 10; i += 2) {
+                System.out.print("\n");
+            }
+            for(int j = 22; j >= 2; j -=4) {
+                System.out.print("!");
+            }
+            for(int k = -2; k <= 10; k += 2) {
+                System.out.print("/");
             }
             System.out.println();
         }
