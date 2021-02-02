041b67a429354aabc9fcb24e0842e0a3d3c79cf8
Vinh Mao <vinhxmao87@gmail.com>
2021-02-01 22:38:05 -0800

    slashFigure code added

---
+++ ForLoops.java
@@ -0,0 +1,20 @@
+public class ForLoops {
+    public static void main(String args[]) {
+        slashFigure();
+    }
+
+    public static void slashFigure() {
+        for(int x = 0; x < 6; x++) {
+            for(int i = 0; i <= 10; i += 2) {
+                System.out.print("n\\");
+                for(int j = 22; j >= 2; j -=4) {
+                    System.out.print("!");
+                    for(int k = 0; k <= 10; k += 2) {
+                        System.out.print("/");
+                    }
+                }
+            }
+            System.out.println();
+        }
+    }
+}
