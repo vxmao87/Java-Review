69d30d699028d0bcaf00820cbdc7ad8c76726c70
Vinh Mao <vinhxmao87@gmail.com>
2021-02-01 23:08:37 -0800

    starSlashFigure: code condensed for readability

--- ForLoops.java
+++ ForLoops.java
@@ -19,26 +19,30 @@ public class ForLoops {
         }
     }
 
+    public static void printObject(int times, String character) {
+        for(int i = 0; i < times; i++) {
+            System.out.print(character);
+        }
+    }
+
     public static void starSlashFigure() {
         for(int x = 1; x <= 7; x++) {
-            for(int i = 0; i < 7 - x; i++) {
-                System.out.print("*");
-            }
-            for(int j = 0; j < x; j++) {
-                System.out.print(" ");
-            }
+            printObject(7 - x, "*");
+
+            printObject(x, " ");
+
             for(int k = 0; k < 14 - 2 * x; k++) {
                 System.out.print("/");
             }
+
             for(int m = 0; m < -2 + 2 * x; m++) {
                 System.out.print("\\");
             }
-            for(int n = 0; n < x; n++) {
-                System.out.print(" ");
-            }
-            for(int p = 0; p < 7 - x; p++) {
-                System.out.print("*");
-            }
+
+            printObject(x, " ");
+
+            printObject(7 - x, "*");
+
             System.out.println();
         }
     }
