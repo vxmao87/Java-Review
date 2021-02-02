e730bd6f45295d3782a5feb3ecbf2f72a8cedfe3
Vinh Mao <vinhxmao87@gmail.com>
2021-02-02 11:32:56 -0800

    staircases: code initiated

---
+++ forLoops/staircases.java
@@ -0,0 +1,13 @@
+package forLoops;
+
+public class staircases {
+    
+    public static final int STEPS = 5;
+    public static void main(String args[]) {
+        for(int x = 1; x <= 5; x++) {
+            for(int a = 0; a < 20; a++) {
+                System.out.print(" ");
+            }
+        }
+    }
+}
