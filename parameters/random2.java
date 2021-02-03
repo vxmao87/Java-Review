f5f5ca64dff450a8e157cb5125bf81a9af7070b6
Vinh Mao <vinhxmao87@gmail.com>
2021-02-02 17:19:58 -0800

    review of Scanner for Java

---
+++ parameters/random2.java
@@ -0,0 +1,37 @@
+package parameters;
+
+import java.util.*;
+
+public class random2 {
+    public static void main(String args[]) {
+        Scanner console = new Scanner(System.in);
+
+        vertical("what happens now");
+        System.out.println();
+        printReverse("what happens now");
+        System.out.println();
+        inputBirthday(console);
+    }
+    
+    public static void vertical(String sentence) {
+        for(int i = 0; i < sentence.length(); i++) {
+            System.out.println(sentence.charAt(i));
+        }
+    }
+
+    public static void printReverse(String sentence) {
+        for(int i = sentence.length() - 1; i >= 0; i--) {
+            System.out.print(sentence.charAt(i));
+        }
+    }
+
+    public static void inputBirthday(Scanner console) {
+        System.out.println("On what day of the month were you born? ");
+        int day = console.nextInt();
+        System.out.println("What month were you born?");
+        String month = console.next();
+        System.out.println("What year were you born?");
+        int year = console.nextInt();
+        System.out.println("You were born on " + month + " " + day + ", " + year + "!");
+    }
+}
