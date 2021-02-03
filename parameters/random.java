488724d31ac7450a974c82e165d750a3a0b51678
Vinh Mao <vinhxmao87@gmail.com>
2021-02-02 16:49:52 -0800

    more Java parameter work

--- parameters/random.java
+++ parameters/random.java
@@ -8,6 +8,8 @@ public class random {
         System.out.println();
         printPowersOfN(-2, 8);
         System.out.println();
+        printSquare(3, 7);
+        printSquare(7, 3); // Produces no output
     }
 
     public static void printNumbers(int num) {
@@ -26,5 +28,21 @@ public class random {
         for(int i = 0; i <= b; i++) {
             System.out.print((int) Math.pow(a, i) + " ");
         }
+    }
+
+    public static void printSquare(int min, int max) {
+        for(int x = min; x <= max; x++) {
+            for(int i = x; i <= max; i++) {
+                System.out.print(i);
+            }
+            for(int j = min; j < x; j++) {
+                System.out.print(j);
+            }
+            System.out.println();
+        }
+    }
+
+    public static void printGrid(int row, int column) {
+        for(int x = )
     }
 }
