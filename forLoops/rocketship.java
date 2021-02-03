9b07d4079fb784e2513ea6bf53e10e95511957d7
Vinh Mao <vinhxmao87@gmail.com>
2021-02-02 16:21:34 -0800

    rocketship: class constant addition in process

--- forLoops/rocketship.java
+++ forLoops/rocketship.java
@@ -14,27 +14,38 @@ public class rocketship {
         drawTop();
     }
 
+    // Main method for printing characters that make up Space Needle
+    public static void drawObject(int times, String character) {
+        for(int i = 0; i < times; i++) {
+            System.out.print(character);
+        }
+    }
+
     public static void drawTop() {
         for(int x = 1; x <= SIZE + 2; x++) {
-            for(int i = 0; i < 2 * SIZE - x; i++) {
-                System.out.print(" ");
-            }
-            for(int j = 0; j < x; j++) {
-                System.out.print("/");
-            }
+            drawObject(2 * SIZE - x, " ");
+            // for(int i = 0; i < 2 * SIZE - x; i++) {
+            //     System.out.print(" ");
+            // }
+            drawObject(x, "/");
+            // for(int j = 0; j < x; j++) {
+            //     System.out.print("/");
+            // }
             System.out.print("**");
-            for(int k = 0; k < x; k++) {
-                System.out.print("\\");
-            }
+            drawObject(x, "\\");
+            // for(int k = 0; k < x; k++) {
+            //     System.out.print("\\");
+            // }
             System.out.println();
         }
     }
 
     public static void drawLine() {
         System.out.print("+");
-        for(int i = 0; i < 2 * SIZE; i++) {
-            System.out.print("=*");
-        }
+        drawObject(2 * SIZE, "=*");
+        // for(int i = 0; i < 2 * SIZE; i++) {
+        //     System.out.print("=*");
+        // }
         System.out.print("+");
         System.out.println();
     }
@@ -42,21 +53,26 @@ public class rocketship {
     public static void drawTopBody() {
         for(int x = 1; x <= SIZE; x++) {
             System.out.print("|");
-            for(int i = 0; i < SIZE - x; i++) {
-                System.out.print(".");
-            }
-            for(int j = 0; j < x; j++) {
-                System.out.print("/\\");
-            }
-            for(int k = 0; k < 2 * SIZE - 2 * x; k++) {
-                System.out.print(".");
-            }
-            for(int a = 0; a < x; a++) {
-                System.out.print("/\\");
-            }
-            for(int b = 0; b < SIZE - x; b++) {
-                System.out.print(".");
-            }
+            drawObject(SIZE - x, ".");
+            // for(int i = 0; i < SIZE - x; i++) {
+            //     System.out.print(".");
+            // }
+            drawObject(x, "/\\");
+            // for(int j = 0; j < x; j++) {
+            //     System.out.print("/\\");
+            // }
+            drawObject(2 * SIZE - 2 * x, ".");
+            // for(int k = 0; k < 2 * SIZE - 2 * x; k++) {
+            //     System.out.print(".");
+            // }
+            drawObject(x, "/\\");
+            // for(int a = 0; a < x; a++) {
+            //     System.out.print("/\\");
+            // }
+            drawObject(SIZE - x, ".");
+            // for(int b = 0; b < SIZE - x; b++) {
+            //     System.out.print(".");
+            // }
             System.out.print("|");
             System.out.println();
         }
