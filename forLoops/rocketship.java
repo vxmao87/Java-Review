8e6e5bf8bb2c06042bc5c25a0fe0bc8f664d4db7
Vinh Mao <vinhxmao87@gmail.com>
2021-02-02 16:26:55 -0800

    rocketship: code cleanup, comments added

--- forLoops/rocketship.java
+++ forLoops/rocketship.java
@@ -1,7 +1,7 @@
 package forLoops;
 
 public class rocketship {
-    public static final int SIZE = 6;
+    public static final int SIZE = 3;
     public static void main(String args[]) {
         drawTop();
         drawLine();
@@ -21,86 +21,62 @@ public class rocketship {
         }
     }
 
+    // Prints the top point of the rocket
     public static void drawTop() {
         for(int x = 1; x <= SIZE + 2; x++) {
             drawObject(2 * SIZE - x, " ");
-            // for(int i = 0; i < 2 * SIZE - x; i++) {
-            //     System.out.print(" ");
-            // }
+
             drawObject(x, "/");
-            // for(int j = 0; j < x; j++) {
-            //     System.out.print("/");
-            // }
+
             System.out.print("**");
             drawObject(x, "\\");
-            // for(int k = 0; k < x; k++) {
-            //     System.out.print("\\");
-            // }
+
             System.out.println();
         }
     }
 
+    // Prints line separating rocket parts
     public static void drawLine() {
         System.out.print("+");
         drawObject(2 * SIZE, "=*");
-        // for(int i = 0; i < 2 * SIZE; i++) {
-        //     System.out.print("=*");
-        // }
+
         System.out.print("+");
         System.out.println();
     }
 
+    // Prints top half of body
     public static void drawTopBody() {
         for(int x = 1; x <= SIZE; x++) {
             System.out.print("|");
             drawObject(SIZE - x, ".");
-            // for(int i = 0; i < SIZE - x; i++) {
-            //     System.out.print(".");
-            // }
+
             drawObject(x, "/\\");
-            // for(int j = 0; j < x; j++) {
-            //     System.out.print("/\\");
-            // }
+
             drawObject(2 * SIZE - 2 * x, ".");
-            // for(int k = 0; k < 2 * SIZE - 2 * x; k++) {
-            //     System.out.print(".");
-            // }
+
             drawObject(x, "/\\");
-            // for(int a = 0; a < x; a++) {
-            //     System.out.print("/\\");
-            // }
+
             drawObject(SIZE - x, ".");
-            // for(int b = 0; b < SIZE - x; b++) {
-            //     System.out.print(".");
-            // }
+
             System.out.print("|");
             System.out.println();
         }
     }
 
+    // Prints bottom half of body
     public static void drawBottomBody() {
         for(int x = 1; x <= SIZE; x++) {
             System.out.print("|");
             drawObject(x - 1, ".");
-            // for(int i = 0; i < x - 1; i++) {
-            //     System.out.print(".");
-            // }
+
             drawObject(-x + SIZE + 1, "\\/");
-            // for(int j = 0; j < -x + SIZE + 1; j++) {
-            //     System.out.print("\\/");
-            // }
+
             drawObject(x * 2 - 2, ".");
-            // for(int k = 0; k < x * 2 - 2; k++) {
-            //     System.out.print(".");
-            // }
+
             drawObject(-x + SIZE + 1, "\\/");
-            // for(int a = 0; a < -x + SIZE + 1; a++) {
-            //     System.out.print("\\/");
-            // }
+
             drawObject(x - 1, ".");
-            // for(int b = 0; b < x - 1; b++) {
-            //     System.out.print(".");
-            // }
+
             System.out.print("|");
             System.out.println();
         }
