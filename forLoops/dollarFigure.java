36dfaf343f2015b14596bdc17e042f7a3f5d2560
Vinh Mao <vinhxmao87@gmail.com>
2021-02-03 23:58:45 -0800

    dollarFigure: small errors implementing class constant

--- forLoops/dollarFigure.java
+++ forLoops/dollarFigure.java
@@ -1,8 +1,9 @@
 package forLoops;
 
 public class dollarFigure {
+    public static final int SIZE = 7;
     public static void main(String args[]) {
-        for(int x = 1; x <= 6; x++) {
+        for(int x = 1; x <= SIZE; x++) {
             for(int i = 0; i < 2 * x - 2; i++) {
                 System.out.print("*");
             }
