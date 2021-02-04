b56c7bd79ff098a54a001688b91cb889298c1222
Vinh Mao <vinhxmao87@gmail.com>
2021-02-04 00:01:27 -0800

    dollarFigure: class constant successfully implemented

--- forLoops/dollarFigure.java
+++ forLoops/dollarFigure.java
@@ -1,19 +1,19 @@
 package forLoops;
 
 public class dollarFigure {
-    public static final int SIZE = 7;
+    public static final int SIZE = 9;
     public static void main(String args[]) {
         for(int x = 1; x <= SIZE; x++) {
             for(int i = 0; i < 2 * x - 2; i++) {
                 System.out.print("*");
             }
-            for(int j = 0; j < 8 - x; j++) {
+            for(int j = 0; j < SIZE + 1 - x; j++) {
                 System.out.print("$");
             }
-            for(int k = 0; k < 16 - 2 * x; k++) {
+            for(int k = 0; k < (SIZE * 2 + 2) - 2 * x; k++) {
                 System.out.print("*");
             }
-            for(int j = 0; j < 8 - x; j++) {
+            for(int j = 0; j < SIZE + 1 - x; j++) {
                 System.out.print("$");
             }
             for(int i = 0; i < 2 * x - 2; i++) {
